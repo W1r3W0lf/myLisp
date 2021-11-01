3fe8dd21faac7be0b580b22bf0677fcb7f72159f
W1r3W0lf <apple.link.level@gmail.com>
2021-10-31 23:16:37 -0600

    created symtable

--- symtable.c
+++ symtable.c
@@ -1,1 +1,63 @@
 #include "symtable.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include "ast.h"
+
+
+sym_node* sym_define(sym_node* table, char* symbol, ast_node* value){
+
+	// If no exsisting table skip looking in the table.
+	if (table){
+	// Look for symbol in table
+		sym_node* node = table;
+		while( node->next ){
+			// If in the table update the value
+			if ( strcmp(symbol, node->symbol) ){
+				node->value->ref_count--;
+				ast_free(node->value); // Try and free up the now orphied ast
+				node->value = value;
+				return table;
+			}
+			node = node->next;
+		}
+	}
+
+	// If not in table create new node.
+	sym_node *new_node = malloc(sizeof(sym_node));
+	new_node->symbol = strdup(symbol);
+
+	// Add the new node to the front of the list.
+	new_node->next = table;
+
+	return new_node;
+}
+
+ast_node* sym_lookup(sym_node* table, char* symbol){
+
+	// Table must not be null
+	assert(table);
+
+	sym_node* node = table;
+	while( node->next ){
+		if (strcmp(symbol, node->symbol))
+			return node->value;
+	}
+	return NULL;
+}
+
+int sym_free(sym_node* table){
+
+	// Table must not be null
+	assert(table);
+
+	sym_node* node = table;
+	do {
+		free(node->symbol);
+		ast_free(node->value);
+		node = node->next;
+	} while( node );
+
+	return 0;
+}
--- symtable.h
+++ symtable.h
@@ -3,7 +3,16 @@
 
 #include "ast.h"
 
+typedef struct sym_node{
+	char* symbol;
+	ast_node* value;
+	struct sym_node *next;
+} sym_node;
 
-int new_define();
+sym_node* sym_define(sym_node* table, char* symbol, ast_node* value);
+
+ast_node* sym_lookup(sym_node* table, char* symbol);
+
+int sym_free(sym_node* table);
 
 #endif
